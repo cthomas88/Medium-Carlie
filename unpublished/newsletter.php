2dbb7fc840f7bd06ae9add9c005809bd9a57cb61
Carlie Thomas <carlierobinson88@yahoo.com>
2019-08-27 15:06:23 +0100

    Add styling to second header section
    Also added php text to newsletter php file

---
+++ unpublished/newsletter.php
@@ -0,0 +1,19 @@
+<?php
+
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+
+    $email_from = 'Medium Carlie';
+    $email_subject = 'You have a new newsletter sign up';
+    $email_body = "Name: $name.\n".
+                    "Email Address: $email.\n";
+    
+    $to = "info@mediumcarlie.co.uk";
+    $headers = "From: $email_from \r\n";
+    $headers .= "Reply-To: $email \r\n";
+
+    mail($to,$email_subject,$email_body,$headers);
+
+    header("location: thanksnewsletter.html")
+
+?>
