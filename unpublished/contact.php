e57962e4508887219afb94dc55c5ae60534f756e
Carlie Thomas <carlierobinson88@yahoo.com>
2019-08-28 21:38:17 +0100

    Restyle all fonts
    Add PHP to contact page, add newsletter thanks and
    contact page thanks details and styling. Change footer
    hover effects.

---
+++ unpublished/contact.php
@@ -0,0 +1,25 @@
+<?php
+
+    $name = $_POST['name'];
+    $lname = $_POST['lname'];
+    $email = $_POST['email'];
+    $number = $_POST['number'];
+    $message = $_POST['message'];
+
+    $email_from = "Medium Carlie";
+    $email_subject = "You have a new submission form from Carlie's Angels";
+    $email_body = "Name: $name.\n".
+                    "Last Name: $lname.\n".
+                    "Email Address: $email.\n". 
+                    "Telephone Number: $number.\n". 
+                    "Message: $message.\n";
+    
+    $to = "info@mediumcarlie.co.uk";
+    $headers = "From: $email_from \r\n";
+    $headers .= "Reply-To: $email \r\n";
+
+    mail($to,$email_subject,$email_body,$headers);
+
+    header("location: thankscontact.html");
+
+    ?>
